7134e991fbe1025d8f8dd454fdc9c1ce0a5cb18d
ChoukseyKhushbu <khushbu25chouksey@gmail.com>
2020-06-25 22:51:52 +0530

    fixed lazy loading image issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { HashRouter } from 'react-router-dom';
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { HashRouter } from "react-router-dom";
+import "./index.scss";
 
-ReactDOM.render(<HashRouter>
-  <App />
-</HashRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
